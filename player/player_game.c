5ca6c9cb25aacc624ce6e30260f6d5bb4cd17237
Tetiana <78310503+TetianaDubovyk@users.noreply.github.com>
2023-08-07 11:54:08 +0300

    updated calculating the length of a map line when reading from a file

--- player/player_game.c
+++ player/player_game.c
@@ -41,7 +41,7 @@ bool load_map (char file_name[MAX_STRING_SIZE], struct map_state *map){
 
     while(fgets(map_line, MAX_SIZE_OF_MAP, map_file_ptr)) {
 
-        map->map_width = sizeof(map_line);
+        map->map_width = strlen(map_line);
 
         for(int i = 0; i <= map->map_width; i++ ){
 
